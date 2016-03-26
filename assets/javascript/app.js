027e49b93f7d5997c126505f906c2250571644b7
Ahmed Haque <afhaque@rice.edu>
2016-03-25 20:52:37 -0400

    Revised code! Working life expectancy + revised repeats + revised undefined

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,24 +2,33 @@
  
  
 $(document).ready(function(){
+
   //click function
   $('#submit').click(function(){
-  	function makeElem(type, data, elemToappendTo){
-  		var childElem = $(type).text(data);
 
-  		elemToappendTo.append(childElem);
-  	}
+    displayCountryInfo();
+    return false;
 
-  	function displayCountryInfo(){
-  	   //gives value for selected option
-  	   var selectedCountry = ($('#country :selected').text()).toLowerCase();
-  	     //alert(selectedCountry);
-  	       //console.log(selectedCountry);
-  	
-  	   var selectBoxes =($('#selectBoxes :selected').text()).toLowerCase();
-  	    // alert(selectBoxes); 
+  });
 
-  	   var selectBoxes2 =($('#selectBoxes2 :selected').text()).toLowerCase();
+    function makeElem(type, data, elemToappendTo){
+      var childElem = $(type).text(data);
+
+      elemToappendTo.append(childElem);
+    }
+
+    function displayCountryInfo(){
+
+
+       //gives value for selected option
+       var selectedCountry = ($('#country :selected').text());
+         //alert(selectedCountry);
+           //console.log(selectedCountry);
+    
+       var selectBoxes =($('#selectBoxes :selected').text()).toLowerCase();
+       // alert(selectBoxes); 
+
+       var selectBoxes2 =($('#selectBoxes2 :selected').text()).toLowerCase();
          //alert(selectBoxes2);
 
        var selectBoxes3 =($('#selectBoxes3 :selected').text()).toLowerCase();
@@ -30,8 +39,11 @@ $(document).ready(function(){
 
        var selectBoxes5 =($('#selectBoxes5 :selected').text()).toLowerCase();
          //alert(selectBoxes5);
+
+        var selectBoxes6 =($('#selectBoxes6 :selected').text()).toLowerCase();
+
          //restcountry api  
-  	   var queryURL = ("https://restcountries.eu/rest/v1/name/" + selectedCountry );
+       var queryURL = ("https://restcountries.eu/rest/v1/name/" + selectedCountry );
      
        //console.log(queryURL);
          //giphy api
@@ -44,7 +56,7 @@ $(document).ready(function(){
 
    $.ajax({url: queryURL, method: 'GET'}).done(function(response){
 
-  	 console.log(response[0][selectBoxes]);
+     console.log(response[0][selectBoxes]);
 
      console.log(response[0].Capital);
   
@@ -52,19 +64,33 @@ $(document).ready(function(){
       
       var countryDiv = $('<div class = "countryDiv" >');
 
+      countryDiv.empty();
+      $('.results-container').empty();
+
          makeElem('<p>', "Country: " + selectedCountry,countryDiv);
-      
-         makeElem('<p>', selectBoxes +": " + (response[0][selectBoxes]),countryDiv);
+        
+         if(response[0][selectBoxes] != null) {
+           makeElem('<p>', selectBoxes +": " + (response[0][selectBoxes]),countryDiv);
+         }      
          
-         makeElem('<p>', selectBoxes2 +": " + (response[0][selectBoxes2]),countryDiv);
+         if(response[0][selectBoxes2] != null) {
+           makeElem('<p>', selectBoxes2 +": " + (response[0][selectBoxes2]),countryDiv);
+         }
          
-         makeElem('<p>', selectBoxes3 +": " + (response[0][selectBoxes3]),countryDiv);
+         if(response[0][selectBoxes3] != null) {
+           makeElem('<p>', selectBoxes3 +": " + (response[0][selectBoxes3]),countryDiv);
+         }
 
-         makeElem('<p>', selectBoxes4 +": " + (response[0][selectBoxes4]),countryDiv);
+        if(response[0][selectBoxes4] != null) {
+           makeElem('<p>', selectBoxes4 +": " + (response[0][selectBoxes4]),countryDiv);          
+        } 
 
+        if(response[0][selectBoxes5] != null){
          makeElem('<p>', selectBoxes5 +": " + (response[0][selectBoxes5]),countryDiv)
+        }
+
        // appending results to main page
-      $('.container').append(countryDiv);
+      $('.results-container').append(countryDiv);
     
     });
 
@@ -83,25 +109,70 @@ $(document).ready(function(){
 
        var imageDiv = $('<div class ="imageDiv">');
 
+       imageDiv.empty();
+       $('.image-container').empty();
+
        var countryImage = $('<img>');
 
        countryImage.attr('src',result[i].images.fixed_height_still.url);
       
        imageDiv.append(countryImage);
 
-       $('.container').append(imageDiv);
+       $('.image-container').append(imageDiv);
 
       }
    })
      
  //-----------------------------------------------//
+
+var lifeExpectancy = "life expectancy at birth";
+if (selectBoxes == lifeExpectancy || selectBoxes2 == lifeExpectancy || selectBoxes3 == lifeExpectancy || selectBoxes4 == lifeExpectancy || selectBoxes5 == lifeExpectancy || selectBoxes6 == lifeExpectancy ) {
+
+
+//  Racquel's code for UN Data 
+  // make the AJAX request
+
+// http://api.undata-api.org/WHO/Life%20expectancy%20at%20birth/Germany/records?app_id=9ed2c241&app_key=09a895dafcf6e70bad03bef56317f454
+
+ $.getJSON('http://api.undata-api.org/WHO/' + "Life expectancy at birth" + '/' +
+  selectedCountry +
+  '/records?app_id=9ed2c241&app_key=09a895dafcf6e70bad03bef56317f454',
+  function(data) {
+    console.log(data);
+
+    // do all this on success       
+    var items = [],
+      $ul;
+    $.each(data, function(key, val) {
+      //iterate through the returned data and build a list
+        items.push('<li class="table" id="' + key + '"><span>' + val.gender +
+          '</span><span> life expectancy was ' + val.value +
+          ' years old</span><span> in the year ' + val.year +
+          '.</span></li>');
+    });
+    // append list to page
+
+    $("#contentTwo").empty();
+
+    $ul = $('<ul />').appendTo('#contentTwo');
+    //append list items to list
+    $ul.append(items);
+  });
+
+
+
+}
+
+  
+
+
+
+ //-----------------------------------------------//
    
-   return false;
 
    }
-  	 $(document).on('click', displayCountryInfo);
   });
-});
+
  
  
 //--------------------------------------------------------------------
@@ -120,7 +191,7 @@ var number = 1;
             "<option>Capital</option> " +
             "<option>Area</option> " +
             "<option>Population</option> " +
-            
+            "<option>Life Expectancy at Birth</option>" +
             "</select> " +
            "<br />"
             
